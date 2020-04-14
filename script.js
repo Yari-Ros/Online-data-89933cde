44dd009b7356ee7d4001a335357568b9e3c916bf
Yari-Ros <2118088@talnet.nl>
2020-04-14 13:19:47 +0200

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,2 @@
+var persoon = {Naam:"Zara", score: Number(50), begindatum: "21 januari 2016", vrienden: ["Mika", "Josef", "Maria", "Sumail", "Lotte"], gekwalificeerd: Boolean('waar')};
+console.log(persoon);
